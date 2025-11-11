d029a88840fe4c3c35e17da6bf156be535eb3b19
gdac <gabrieldalcs@gmail.com>
2025-11-11 10:27:48 -0300

    add initial strucuture

---
+++ llm/llm.py
@@ -0,0 +1,85 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from typing import List, Dict, Optional
+
+class LocalModelTransformers():
+    def __init__(self, model_name, device: str = "auto"):
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model_name,
+            padding_side="left"
+        )
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            device_map=device,
+            dtype=torch.bfloat16
+        )
+        if self.tokenizer.pad_token_id is None:
+            self.tokenizer.pad_token_id = self.tokenzier.eos_token_id
+            self.model.config.pad_token_id = self.model.config.eos_token_id
+        
+    def generate(
+            self,
+            user_prompt: str,
+            system_prompt: str = None,
+            max_tokens: int = 4096,
+            temperature: float = 0.7,
+            condition: str = ""
+    ):
+        output_ids = None
+        inputs = None
+        generation_params = {
+            "max_new_tokens": max_tokens,
+            "pad_token_id": self.tokenizer.eos_token_id
+        }
+        if temperature > 0:
+            generation_params["do_sample"] = True
+            generation_params["temperature"] = temperature
+        with torch.inference_mode():
+            if getattr(self.tokenizer, "chat_template", None):
+                try:
+                    messages = [
+                        {"role": "system", "content": system_prompt},
+                        {"role": "user", "content": user_prompt}
+                    ] if system_prompt else [{"role": "user", "content": user_prompt}]
+                    plain_text = self.tokenizer.apply_chat_template(
+                        messages,
+                        tokenize=False,
+                        add_generation_prompt=True
+                    )
+                    plain_text += condition
+                    inputs = self.tokenizer(plain_text, return_tensors="pt")
+                except:
+                    plain_text = f"{system_prompt}\n\n{user_prompt}" if system_prompt else user_prompt
+                    plain_text += condition
+                    inputs = self.tokenizer.apply_chat_template(
+                        messages,
+                        tokenize=False,
+                        add_generation_prompt=True
+                    )
+                    inputs = self.tokenizer(plain_text, return_tensors="pt")
+            else:
+                plain_text = f"{system_prompt}\n\n{user_prompt}" if system_prompt else user_prompt
+                plain_text += condition
+                inputs = self.tokenizer(plain_text, return_tensors="pt")
+            
+            
+            output_ids = self.model_generate(**inputs, **generation_params)
+            input_length = inputs['input_ids'].shape[1]
+            generated_tokens = output_ids[0][input_length:]
+            final_response = self.wrapper(
+                self.tokenizer.decode(
+                    generated_tokens,
+                    skip_special_tokens=True
+                ).strip()
+            )
+            del inputs
+            del output_ids
+            torch.cuda.empty_cache()
+            return final_response
+        
+    def wrapper(self, response: str):
+        tag = "[END OF THE PLAY]"
+        if tag in response:
+            return response.split(tag)[0]
+        return response
+        
---
+++ llm/prompt_template.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+## Prompts diferentes
+## 1 fase-> alocação de tropas -> Prompt -> Onde botar as tropas e quantas
+## 2 fase -> ataque. Prompt -> quem atacar
+## 2 fase -> Precisamos de prompts dinamicos e chamadas dinamicas para cada decisão de ataque
+## O bot vai falar que quer atackar x. Depois do ataque de X, dependendo do que aconteceu, o bot pode decidir que 
+## quer continuar o ataque ou não, ou que quer mudar os ataques.
+## 3 fase: fortificação: Um único prompt
+
+@dataclass
+class PromptTemplate:
+    system_prompt: str
+    user_prompt: str
+    condition: str
+    temperature: float = 0.7
+    max_tokens: int = 4096
+
+@dataclass
+class AIEasy:
+    user_prompt = ""
+    system_prompt = ""
+    @staticmethod
+    def get_easy_prompt() -> PromptTemplate:
+        pass
+
+@dataclass
+class AIMedium:
+    user_prompt = ""
+    system_prompt = ""
+    @staticmethod
+    def get_medium_prompt() -> PromptTemplate:
+        pass
+
+@dataclass    
+class AIHard:
+    user_prompt = ""
+    system_prompt = ""
+    @staticmethod
+    def get_hard_prompt() -> PromptTemplate:
+        pass
+
+class AIPromptManager:
+    def get_prompt(dif: str):
+        if dif == "easy":
+            AIEasy.get_easy_prompt()
+        elif dif == "medium":
+            AIMedium.get_medium_prompt()
+        elif dif == "hard":
+            AIHard.get_hard_prompt()
+        else:
+            ## Fallback
+            AIMedium.get_medium_prompt()
+
